b09e6f2be85efafb1e36b533db2fdc70b4ae7bf8
eddyperea05 <eddyperea05@gmail.com>
2021-05-24 12:37:00 -0500

    A

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const hbs = require("express-hbs")
+const path = require("path")
+const body = require("body-parser")
+const bodyParser = require('body-parser')
+const funciones = require("./helpers/index")
+const app = express()
+
+app.listen(3000)
